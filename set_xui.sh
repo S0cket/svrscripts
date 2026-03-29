4c73e36cf85d9f1637ca8f1aaae4f783080bcd49
S0cket <rus.swordsman@gmail.com>
2026-03-29 14:30:40 +0400

    1

--- set_xui.sh
+++ set_xui.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 SERVER_IP=
-SSH_PORT=12348
-SSH_TCP_FORWARDING=
+SSH_PORT=1234
+SSH_TCP_FORWARDING=1
 UFW=0
 
 XUI_PATH=
-XUI_PORT=12346
-XUI_USER=test
-XUI_PASSWORD=testpass
+XUI_PORT=1235
+XUI_USER=root
+XUI_PASSWORD=toor001
 XUI_URL="https://github.com/MHSanaei/3x-ui/releases/download/v2.8.11/x-ui-linux-amd64.tar.gz"
 
 XUI_DIR="/opt"
@@ -74,5 +74,122 @@ cd "$XUI_HOME"
 chmod a+x x-ui.sh
 
 
+DATA=$(expect <<'EOF'
+spawn ./x-ui.sh
+
+set timeout 10
+expect {
+	-re {enter.*selection} {}
+	timeout {interact}
+}
+send "1\r"
+
+set timeout 300
+expect {
+	-re {customize.*port} {}
+	timeout {interact}
+}
+set timeout 10
+if {"$XUI_PORT" != ""} {
+	send "y\r"
+	expect {
+		-re {port} {}
+		timeout {interact}
+	}
+	send "$XUI_PORT\r"
+}
+else {
+	send "n\r"
+}
+
+expect {
+	-re {SSL.*Choose} {}
+	timeout {interact}
+}
+send "3\r"
+
+expect {
+	-re {certificate.*issued.*for} {}
+	timeout {interact}
+}
+send "$SERVER_IP\r"
+
+expect {
+	-re {\.crt} {}
+	timeout {interact}
+}
+send "$XUI_HOME/ssl/server.crt\r"
+
+expect {
+	-re {\.key} {}
+	timeout {interact}
+}
+send "$XUI_HOME/ssl/server.key\r"
+
+expect {
+	-re {main.*menu} {}
+	timeout {interact}
+}
+send "\r"
+
+expect {
+	-re {enter.*selection} {}
+	timeout {interact}
+}
+send "6\r"
+
+expect {
+	-re {sure.*username.*password} {}
+	timeout {interact}
+}
+send "y\r"
+
+expect {
+	-re {username} {}
+	timeout {interact}
+}
+send "$XUI_USER\r"
+
+expect {
+	-re {password} {}
+	timeout {interact}
+}
+send "$XUI_PASSWORD\r"
+
+expect {
+	-re {disable.*two-factor} {}
+	timeout {interact}
+}
+send "y\r"
+
+expect {
+	-re {restart} {}
+	timeout {interact}
+}
+send "y\r"
+
+
+expect {
+	-re {main.*menu} {}
+	timeout {interact}
+}
+send "\r"
+
+expect {
+	-re {enter.*selection} {}
+	timeout {interact}
+}
+send "10\r"
+
+expect {
+	eof {puts $expect_out(buffer)}
+	timeout {interact}
+}
+
+
+EOF
+)
+
+echo $DATA
 
 cd "$OLD_PWD"
